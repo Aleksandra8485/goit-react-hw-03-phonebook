8770089eebc39f0f18c1d47dee1a87f13b9b91ff
Aleksandra <a.walczak1984@gmail.com>
2023-06-25 21:58:42 +0200

    change in app.jsx localstorage works

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
@@ -6,15 +6,31 @@ import ContactList from './ConctactList/ContactList';
 import styles from './App.module.css';
 
 const App = () => {
-  const initialContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
+  const initialContacts = useMemo(
+    () => [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    []
+  );
 
-  const [contacts, setContacts] = useState(initialContacts);
-  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('phonebookContacts');
+
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    } else {
+      setContacts(initialContacts);
+    }
+  }, [initialContacts]);
+
+  useEffect(() => {
+    localStorage.setItem('phonebookContacts', JSON.stringify(contacts));
+  }, [contacts]); // Dodaj contacts do tablicy zależności
 
   const addContact = (name, number) => {
     const existingContact = contacts.find(
@@ -36,6 +52,8 @@ const App = () => {
     setContacts(contacts.filter(contact => contact.id !== contactId));
   };
 
+  const [filter, setFilter] = useState('');
+
   const handleFilterChange = event => {
     setFilter(event.target.value);
   };
@@ -56,6 +74,66 @@ const App = () => {
 };
 
 export default App;
+
+// działający kod zadania 2 po refakturyzacji
+// import React, { useState } from 'react';
+// import { nanoid } from 'nanoid';
+// import ContactForm from './ContactForm/ContactForm';
+// import Filter from './Filter/Filter';
+// import ContactList from './ConctactList/ContactList';
+// import styles from './App.module.css';
+
+// const App = () => {
+//   const initialContacts = [
+//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//   ];
+
+//   const [contacts, setContacts] = useState(initialContacts);
+//   const [filter, setFilter] = useState('');
+
+//   const addContact = (name, number) => {
+//     const existingContact = contacts.find(
+//       contact => contact.name.toLowerCase() === name.toLowerCase()
+//     );
+//     if (existingContact) {
+//       alert(`${name} is already in your contacts!`);
+//     } else {
+//       const newContact = {
+//         id: nanoid(),
+//         name: name,
+//         number: number,
+//       };
+//       setContacts([...contacts, newContact]);
+//     }
+//   };
+
+//   const deleteContact = contactId => {
+//     setContacts(contacts.filter(contact => contact.id !== contactId));
+//   };
+
+//   const handleFilterChange = event => {
+//     setFilter(event.target.value);
+//   };
+
+//   const filteredContacts = contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(filter.toLowerCase())
+//   );
+
+//   return (
+//     <div className={styles.appContainer}>
+//       <h1>Phonebook</h1>
+//       <ContactForm addContact={addContact} />
+//       <h2>Contacts</h2>
+//       <Filter filter={filter} handleFilterChange={handleFilterChange} />
+//       <ContactList contacts={filteredContacts} deleteContact={deleteContact} />
+//     </div>
+//   );
+// };
+
+// export default App;
 
 //działajacy kod przed refakturyzacja
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+createRoot(document.getElementById('root')).render(<App />);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
